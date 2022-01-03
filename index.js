b764d269f1c0e9ae7831a5df78cf9137c1a2edeb
Veer Chauhan <r.ranjanchn@gmail.com>
2022-01-04 03:45:21 +0530

    Change

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 
+// env file for constants
 const dotenv = require('dotenv');
+
+//user router
+const userRouter = require('./routes/user')
+
+//create express application
 const app = express();
+
+//config dotenv file to use in app
 dotenv.config()
 
+//DB connection
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('db connection successful');
 }).catch((err) => {
     console.log(err);
 })
 
-// app.get('/api/test', () => {
+//ROuter adding
+app.use('/api/user', userRouter)
 
-//     console.log('Test api link is succesful');
-// })
-
+//Server 
 app.listen(process.env.PORT || 4000,()=> {
-    console.log('server is up',app.PORT);
+    console.log('server is up');
 })
---
+++ routes/user.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+router.get('/',() => {
+    console.log('User router is tyested')
+})
+
+module.exports = router
